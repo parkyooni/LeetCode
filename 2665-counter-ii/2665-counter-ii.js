6abedad63d3de8686a74c574882e4523cc219ada
yooni <86058698+parkyooni@users.noreply.github.com>
2024-05-12 20:14:28 +0900

    Time: 58 ms (61.78%), Space: 51.3 MB (77.30%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,35 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+const createCounter = function(init) {
+    let varInit = init;
+    
+    const increment = function () {
+        varInit ++;
+        return varInit;
+    }
+
+    const decrement = function () {
+        varInit --;
+        return varInit;
+    }
+
+    const reset = function () {
+        varInit = init;
+        return varInit;
+    }
+
+    return {
+        increment,
+        decrement,
+        reset
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
