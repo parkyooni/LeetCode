e3857b963c2f2d6cddfd28d134b346a577dcfb30
yooni <86058698+parkyooni@users.noreply.github.com>
2024-05-14 00:57:13 +0900

    Time: 61 ms (13.47%), Space: 48.3 MB (95.63%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -8,7 +8,7 @@ const filter = function(arr, fn) {
 
     for (let i = 0; i < arr.length; i++) {
         if (fn(arr[i],i)) {
-            filteredArr.push(arr[i])
+            filteredArr.push(arr[i]);
         }
     }
 
