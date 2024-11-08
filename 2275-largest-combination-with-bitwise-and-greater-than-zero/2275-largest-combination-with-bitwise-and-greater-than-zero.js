0b73ba5e1c96f0d5de954c8f2567317b7a6cf29f
Yooni <86058698+parkyooni@users.noreply.github.com>
2024-11-08 23:32:18 +0900

    Time: 38 ms (100.00%), Space: 59.4 MB (16.67%) - LeetHub

--- 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.js
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.js
@@ -6,7 +6,7 @@ const largestCombination = function(candidates) {
     let maxSubsetSize = 0; 
     
     for (let bitPosition = 0; bitPosition < 32; bitPosition++) { 
-        let countOnes = 0; // 현재 비트 위치에서 "1"의 개수를 셀 변수
+        let countOnes = 0;
         for (const number of candidates) {
             if (number & (1 << bitPosition)) countOnes++; 
         }
